e66284bde28d62300e1b6da2983ba9268d512761
Reece Taylor <reece.taylor@live.com>
2023-09-08 08:38:27 +0100

    refactor: complete and make own revised guessing game

--- Guess-The-Number-Game/script.js
+++ Guess-The-Number-Game/script.js
@@ -1,55 +1,48 @@
 'use strict';
 
-/*
+// Function to update text content
+const setTextContent = (selector, content) => {
+  document.querySelector(selector).textContent = content;
+};
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct number!';
+// Capture initial states
+const initialBackgroundColor = '#222';
+const initialNumberWidth = '15rem';
+const initialMessage = 'Start guessing...';
 
-document.querySelector('.number').textContent = 12;
-document.querySelector('.score').textContent = 200;
-
-document.querySelector('.guess').value = 200;
-
-*/
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(`FOR TESTING: secretNumber - ${secretNumber}`);
 let score = 20;
 let highScore = 0;
-// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '❌ No number!';
+    setTextContent('.message', '❌ No number!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct number!';
+    setTextContent('.message', '🎉 Correct number!');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
     if (score > highScore) {
       highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
+      setTextContent('.highscore', highScore);
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
+      setTextContent(
+        '.message',
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low!'
+      );
       score--;
-      document.querySelector('.score').textContent = score;
+      setTextContent('.score', score);
     } else {
-      document.querySelector('.message').textContent = '😞 You lost!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😞 You lost!';
-      document.querySelector('.score').textContent = 0;
+      setTextContent('.message', '😞 You lost!');
+      setTextContent('.score', 0);
     }
   }
 });
@@ -68,16 +61,13 @@ GOOD LUCK 😀
 */
 //////////////////////////////
 
-// Probably a much more efficient way of doing this - could initialize original values first.
 document.querySelector('.again').addEventListener('click', function () {
-  // console.log('Clicked!');
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log(`FOR TESTING: secretNumber - ${secretNumber}`);
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  setTextContent('.number', '?');
+  document.querySelector('body').style.backgroundColor = initialBackgroundColor;
+  document.querySelector('.number').style.width = initialNumberWidth;
   document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = score;
+  setTextContent('.message', initialMessage);
+  setTextContent('.score', score);
 });
