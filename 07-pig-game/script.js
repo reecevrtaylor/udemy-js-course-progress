ae53d119e309035b096759697f0891edb46174c7
Reece Taylor <reece.taylor@live.com>
2023-09-13 09:55:49 +0100

    progress: 07-pig-game - restarting game state

--- 07-pig-game/script.js
+++ 07-pig-game/script.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score1El = document.getElementById('score--0');
 const score2El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -13,24 +17,58 @@ score1El.textContent = 0;
 score2El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Dice func
 btnRoll.addEventListener('click', function () {
-  // Generate random roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // Display dice
-  diceEl.classList.remove('hidden');
-  // Map through each of the dice.png and assign the corresponding dice gen number
-  //   console.log(`./dice-${dice}.png`);
-  diceEl.src = `./dice-${dice}.png`;
+  if (playing) {
+    // Generate random roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // Display dice
+    diceEl.classList.remove('hidden');
+    // Map through each of the dice.png and assign the corresponding dice gen number
+    //   console.log(`./dice-${dice}.png`);
+    diceEl.src = `./dice-${dice}.png`;
 
-  // Check if roll is 1, if true, switch player
-  if (dice !== 1) {
-    // add dice to current score
-    currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER FOR CURRENT PLAYER
-  } else {
-    // switch
+    // Check if roll is 1, if true, switch player
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
   }
 });
