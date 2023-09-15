b83062ba5aa93774600adf672e3a236b35b90949
Reece Taylor <reece.taylor@live.com>
2023-09-15 10:07:00 +0100

    progress: 08-js-behind-scenes: hoisting + tdz

--- 07-pig-game/script.js
+++ 07-pig-game/script.js
@@ -1,14 +1,19 @@
 'use strict';
 
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.getElementById('score--0');
-const score2El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const playersEl = [
+  document.querySelector('.player--0'),
+  document.querySelector('.player--1'),
+];
+const scoresEl = [
+  document.getElementById('score--0'),
+  document.getElementById('score--1'),
+];
+const currentsEl = [
+  document.getElementById('current--0'),
+  document.getElementById('current--1'),
+];
 
 const diceEl = document.querySelector('.dice');
-
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -21,46 +26,39 @@ const init = function () {
   activePlayer = 0;
   playing = true;
 
-  score0El.textContent = 0;
-  score2El.textContent = 0;
-  score0El.textContent = 0;
-  score2El.textContent = 0;
+  scoresEl.forEach((score, index) => {
+    score.textContent = 0;
+    currentsEl[index].textContent = 0;
+  });
 
-  current0El.textContent = 0;
-  current1El.textContent = 0;
   diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+
+  playersEl.forEach((player, index) => {
+    player.classList.remove('player--winner');
+    player.classList.toggle('player--active', index === activePlayer);
+  });
 };
+
 init();
 
 const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentsEl[activePlayer].textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  playersEl.forEach((player, index) => {
+    player.classList.toggle('player--active', index === activePlayer);
+  });
 };
 
-// Dice func
 btnRoll.addEventListener('click', function () {
   if (playing) {
-    // Generate random roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    // Display dice
     diceEl.classList.remove('hidden');
-    // Map through each of the dice.png and assign the corresponding dice gen number
-    //   console.log(`./dice-${dice}.png`);
     diceEl.src = `./dice-${dice}.png`;
 
-    // Check if roll is 1, if true, switch player
     if (dice !== 1) {
-      // add dice to current score
       currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore;
+      currentsEl[activePlayer].textContent = currentScore;
     } else {
       switchPlayer();
     }
@@ -70,19 +68,14 @@ btnRoll.addEventListener('click', function () {
 btnHold.addEventListener('click', function () {
   if (playing) {
     scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
+    scoresEl[activePlayer].textContent = scores[activePlayer];
+
     if (scores[activePlayer] >= 10) {
       playing = false;
-      btnHold.classList.add('disabled');
-      btnRoll.classList.add('disabled');
+      [btnHold, btnRoll].forEach(btn => btn.classList.add('disabled'));
       diceEl.classList.add('hidden');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+      playersEl[activePlayer].classList.add('player--winner');
+      playersEl[activePlayer].classList.remove('player--active');
     } else {
       switchPlayer();
     }
--- 08-js-behind-scenes/script.js
+++ 08-js-behind-scenes/script.js
@@ -1,1 +1,86 @@
 'use strict';
+
+//////////////////////
+// Scope Chain
+//////////////////////
+
+/*
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, are ${age}. You were born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      // Pre ES6 definition - function coped (ignore block)
+      var millennial = true;
+      const firstName = 'Steve'; // Reece or Steve? -> Steve. JS looks for var in scope first, then looks outwards.
+      // const & let vars are block scoped
+      const str = `Oh, you are a millennial, ${firstName}`;
+      console.log(str);
+
+      // block scoped (strict mode - as it should be)
+      function add(a, b) {
+        return a + b;
+      }
+      output = 'NEW OUTPUT';
+    }
+    console.log(millennial);
+    console.log(output); // now = NEW OUTPUT
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Reece';
+calcAge(2001);
+
+*/
+
+//////////////////////
+// Hoisting & TDZ
+//////////////////////
+// Var hoisting
+console.log(me);
+// console.log(job); // TDZ
+// console.log(year); // TDZ
+
+var me = 'Reece';
+let job = 'teacher';
+const year = 1991;
+
+// Function hoisting
+console.log(addDecl(1, 2)); // 3
+// console.log(addExpr(1, 2)); // TDZ
+// console.log(addArrow(1, 2)); // TDZ -> addArrow not a function if set with var instead of const.
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) {
+  deleteShoppingCart();
+}
+var numProducts = 10; // All products deleted -> not 10, its undefined.
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+var y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
